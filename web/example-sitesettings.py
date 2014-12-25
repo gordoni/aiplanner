0599b9e787ea01daca80ee65261cdd2cc0786a35
Gordon Irlam <gordoni@gordoni.com>
2014-12-24 17:02:44 -0800

    Add README files.

---
+++ web/example-sitesettings.py
@@ -0,0 +1,12 @@
+# Django settings that are unique to this site.
+# Includes passwords that shouldn't be put under version control.
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('My Name', 'myname@myemaildomain'),
+)
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '1234567890abcdefghijklmnopqrstuvwxyz1234567890abcd'
