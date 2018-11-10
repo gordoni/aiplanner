a501ceea8f862b7d748631cf28f6933f7ae42315
Gordon Irlam <gordoni@gordoni.com>
2018-11-10 00:14:10 +0000

    ai/baselines: Make ppo1/pposgd_simple callback return whether to finish.

--- ai/baselines/baselines/ppo1/pposgd_simple.py
+++ ai/baselines/baselines/ppo1/pposgd_simple.py
@@ -142,7 +142,9 @@ def learn(env, policy_fn, *,
     assert sum([max_iters>0, max_timesteps>0, max_episodes>0, max_seconds>0])==1, "Only one time constraint permitted"
 
     while True:
-        if callback: callback(locals(), globals())
+        if callback:
+            if callback(locals(), globals()):
+                break
         if max_timesteps and timesteps_so_far >= max_timesteps:
             break
         elif max_episodes and episodes_so_far >= max_episodes:
