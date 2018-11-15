40a257ef6f99e445257d10eb859ce697d4dae59b
Gordon Irlam <gordoni@gordoni.com>
2018-11-15 09:11:16 +0000

    ai: Neatly format web results.

--- ai/angular/src/app/result/result.component.ts
+++ ai/angular/src/app/result/result.component.ts
@@ -31,10 +31,50 @@ export class ResultComponent implements OnInit {
   ) {}
 
   doResults(results) {
-    if (this.mode == 'full' && results.error == 'Results not found.')
+
+    if (this.mode == 'full' && results.error == 'Results not found.') {
+
       this.results = null;
-    else
-      this.results = results;
+
+    } else {
+
+      var asset_classes = '';
+      for (let ac of results.asset_classes) {
+        if (asset_classes)
+          asset_classes += '/';
+        if (ac.endsWith('bonds'))
+          asset_classes += 'bonds';
+        else
+          asset_classes += ac;
+      }
+
+      var asset_allocation = '';
+      var carry = 0;
+      for (let alloc of results.asset_allocation) {
+        if (asset_allocation)
+          asset_allocation += '/';
+        var val = Math.round(alloc * 100 + carry);
+        asset_allocation += val;
+        carry += alloc * 100 - val;
+      }
+
+      this.results = {
+        'error': null,
+        'consume': Math.round(results.consume),
+        'nominal_spias_purchase': results.nominal_spias_purchase == null ? null : Math.round(results.nominal_spias_purchase),
+        'asset_classes': asset_classes,
+        'asset_allocation': asset_allocation,
+        'nominal_bonds_duration': Math.round(results.nominal_bonds_duration),
+        'retirement_contribution': Math.round(results.retirement_contribution),
+        'ce': Math.round(results.ce),
+        'ce_stderr': Math.round(results.ce_stderr),
+        'models_stdev': Math.round(results.models_stdev),
+        'consume_preretirement': Math.round(results.consume_preretirement),
+        'preretirement_ppf': Math.round(results.preretirement_ppf * 100),
+        'ce_low': Math.round(results.ce_low),
+        'data_dir': results.data_dir,
+      }
+    }
   }
 
   handleError(error) {
