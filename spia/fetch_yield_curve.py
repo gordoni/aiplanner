d35eb59e3d07abec8a92c2fd97e4e5cf4db0c0cd
Gordon Irlam <gordoni@gordoni.com>
2019-11-15 04:29:13 +0000

    Add free for non-commercial use and commercial use licenses to ai and spia.

--- spia/fetch_yield_curve.py
+++ spia/fetch_yield_curve.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 
-# Fetch yield curve - Maintain up to date yield curve data
+# SPIA - Income annuity (SPIA and DIA) price calculator
 # Copyright (C) 2015-2018 Gordon Irlam
+#
+# This program may be licensed by you (at your option) under an Open
+# Source, Free for Non-Commercial Use, or Commercial Use License.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
+#
+# This program is free for non-commercial use: you can use and modify it
+# under the terms of the Creative Commons
+# Attribution-NonCommercial-ShareAlike 4.0 International Public License
+# (https://creativecommons.org/licenses/by-nc-sa/4.0/).
+#
+# A Commercial Use License is available in exchange for agreed
+# remuneration.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
