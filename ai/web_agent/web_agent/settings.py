3e47d4be8914ad172b1ee9792334e666d86e03a1
Gordon Irlam <gordoni@gordoni.com>
2021-01-09 01:04:55 +0000

    Prevent spurious GitGuardian security messages.

--- ai/web_agent/web_agent/settings.py
+++ ai/web_agent/web_agent/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(#@wo2f_!!bl_*zlogv#hkakxsjz6_ble+c19xomg=za@db#=x'
+# Need to assign to SECRET_KEY a random 50 character string here.
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
